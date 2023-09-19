3586e4bc2372ac02020f24c5eec949414f966a3e
chill <issakk@126.com>
2023-09-19 14:58:39 +0800

    Update get_clash.py

--- get_clash.py
+++ get_clash.py
@@ -1,33 +1,23 @@
 import requests
 import os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
 # 发起 HTTP 请求并获取网页内容
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
 url = 'https://wanshanziwo.eu.org/clash'  # 替换为你要爬取的网址
-response = requests.get(url)
+response = requests.get(url,headers=headers)
 html = response.text
-
-# Setup
-options = Options()
-#options.add_argument("--headless")
-browser = webdriver.Chrome(options=options)
-# Parse or automation
-browser.get(url)
-
-
-
 # 使用 Beautiful Soup 解析 HTML
-soup = BeautifulSoup(browser.page_source, 'html.parser')
-browser.implicitly_wait(5)
+soup = BeautifulSoup(html, 'html.parser')
+print(html)
 # 提取特定内容
 # 这里只是一个示例，你可以根据需要修改提取的逻辑
 # body > div.container > section:nth-child(2) > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)
 # 使用 CSS 选择器查找指定元素
 element = soup.select_one('html > body > div.container > section:nth-child(2) > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)')
-print(html)
+
 # 提取元素的文本内容
 if element:
     text = element.text
